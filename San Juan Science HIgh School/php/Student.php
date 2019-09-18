3a530f59897a839a7d4016f0bb29d62795852a6d
John Raymer <raymerorale@gmail.com>
2019-09-18 21:33:04 +0800

    Jom changes commited

--- San Juan Science HIgh School/php/Student.php
+++ San Juan Science HIgh School/php/Student.php
@@ -10,7 +10,7 @@ if(!empty($_FILES['files'])){
 	UPLOAD($target_dir);
 }
 else if(!empty($_POST['LastName'])){
-	Insert($db);
+	Insert($db, $target_dir);
 }
 
 
@@ -47,7 +47,7 @@ function UPLOAD($target_dir){
     	// $target_dir = "../pictures/student/";
     	$targetpath = $target_dir . basename($_FILES['files']['name'][$key]);
     	if(move_uploaded_file($value, $targetpath)){
-        	echo true;
+        	echo $target_dir;
     	}
 	}
 }
