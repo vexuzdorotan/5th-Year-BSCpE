a18a4e842c3a5bf502a76c6fba601d8b225612e7
John Raymer <raymerorale@gmail.com>
2019-09-25 20:31:32 +0800

    styled room.php

--- San Juan Science HIgh School/Room.php
+++ San Juan Science HIgh School/Room.php
@@ -3,54 +3,63 @@
 <head>
 	<title>Room</title>
 	<link rel="stylesheet" type="text/css" href="css/Entire.css"/>
-	<style type="text/css">
-		.width100{
-			width: 100%;
-		}
-
-		.width50{
-			width: 50%;
-		}
-	</style>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="css/Room.css" />
+    <link rel="icon" href="pictures\logo.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-	<div style="width: 100%; height: 100%;">
-	<div style="width: 30%;float: left;" id="Room">
-	<h1>ROOM</h1>
-	<p><label for="RoomNum">Room Number: <input type="text" name="RoomNum" id="RoomNum" required/></label></p>
-	<p><label for="RoomName">Room Name: <input type="text" name="RoomName" id="RoomName" required/></label></p>
-	<p><label for="Floor">Floor: <input type="number" name="Floor" id="Floor" min = "1" required/></label></p>
-	<p><label for="Capacity">Capacity: <input type="number" name="Capacity" id="Capacity" min="5" max="50" required/></label></p>
-	<p><label for="Type">Room Type: <select id="Type" name="Type">
-		<option selected="selected">Classroom</option>
-		<option>Laboratory</option>
-		<option>Office</option>
-	</select></label></p>
-	<button id="CreateRoom">Create Room</button>
-	<button id="ResetRoom">Reset</button>
-	<!-- <button id="EditRoom">Edit Room</button> -->
-	<!-- <p><label for="SubName">Subject Name: <input type="text" name="SubName" id="SubName"/></label></p> -->
-	</div>
-	<div style="float: right; width: 70%;">
-	<p><label for="SearchRoom"><input type="search" id="SearchRoom"></label></p>
-		<table id="SearchRoomTable">
-			<thead>
-				<tr>
-				<td id="RoomNum">Room Number</td>
-				<td id="RoomName">Room Name</td>
-				<td id="Floor">Floor</td>
-				<td id="Capacity">Capacity</td>
-				<td id="Type">Type</td>
-				<td></td>
-				</tr>
-			</thead>
-			<tbody>
-				<!-- <td></td> -->
-			</tbody>
-		</table>
-		<!-- <button id="First">Create Room</button>
-		<button id="Las">Reset</button> -->
-	</div>
+	<img src="pictures/logodesign.jpg" class="logodesign">
+	<div class="header mb-3">
+      <legend class="h4 pl-2 pt-3 pb-1 mb-0">MANAGE ROOMS</legend>
+      <div class="menu">
+         <a href="#">Administrator</a>|<a href="#">Menu</a>|<a href="#">Logout</a>
+      </div>
+   	</div>
+   	<div class="my-container row">
+		<div class="col-4" id="Room">
+			<p class="h5 pb-2">Create Room</p>
+			<p class="p-0 m-0"><label for="RoomNum">Room Number: <input type="text" name="RoomNum" id="RoomNum" required/></label></p>
+			<p><label for="RoomName">Room Name: <input type="text" name="RoomName" id="RoomName" required/></label></p>
+			<p"><label for="Floor">Floor: <input class="ml-2" type="number" name="Floor" id="Floor" min = "1" required/></label>
+			<label class="pl-3" for="Capacity">Capacity: <input class="ml-2" type="number" name="Capacity" id="Capacity" min="5" max="50" required/></label></p>
+			<p><label for="Type">Room Type: <select id="Type" name="Type">
+				<option selected="selected">Classroom</option>
+				<option>Laboratory</option>
+				<option>Office</option>
+			</select></label></p>
+		
+			<button class="rounded-pill create-button" id="CreateRoom">Create Room</button>
+			<button class="rounded-pill reset-button" id="ResetRoom">Reset</button>
+			<!-- <button id="EditRoom">Edit Room</button> -->
+			<!-- <p><label for="SubName">Subject Name: <input type="text" name="SubName" id="SubName"/></label></p> -->
+		</div>
+		<div class="col-8">
+			<p class="h5 pb-2">List of Rooms
+			<label class="float-right" for="SearchRoom">
+				<input placeholder="Search for rooms.." class="mt-1 form-control rounded-0 bg-light" type="search" id="SearchRoom">
+			</label></p>
+			<table class="" id="SearchRoomTable">
+				<thead class="dark">
+					<tr>
+					<td scope="col" id="RoomNum">Room Number</td>
+					<td scope="col" id="RoomName">Room Name</td>
+					<td scope="col" id="Floor">Floor</td>
+					<td scope="col" id="Capacity">Capacity</td>
+					<td scope="col" id="Type">Type</td>
+					<td scope="col"></td>
+					</tr>
+				</thead>
+				<tbody>
+					<!-- <td></td> -->
+				</tbody>
+			</table>
+			<!-- <button id="First">Create Room</button>
+			<button id="Las">Reset</button> -->
+		</div>
+		<div class="footer">
+      <p class="footer-text">© 2019 - San Juan Science High School. All Rights Reserved</p>
+   </div>
 	</div>
 	<!-- <div class="width100" id="Section" style="height: 50%;">
 	<div class="width50">
